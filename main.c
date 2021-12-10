f3d7bbcd656e683b77223965d854d1d7c40c885f
mmj-sys <mmj.sys@gmail.com>
2021-12-10 23:31:53 +0330

    discrept a usage if input have not 3 argument

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #define MAX_FILE_SIZE (1024*1024)
 int main(int argc , char *argv[]) {
+  if(argc < 3) {
+    printf("USE [FILE_NAME] [OUT_FILE_NAME] [PASSWORD] ! THE PASSWORD TO BE A NUMBER");
+    exit(1);
+  }
   FILE *fptr;
   char data[MAX_FILE_SIZE];
   fptr = fopen(argv[1] , "rb");

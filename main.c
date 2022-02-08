a8885211d0bdc39f981f73ae6ac0f07038b082ac
mmj-sys <mmj.sys@gmail.com>
2022-02-08 14:19:43 +0330

    debug segment fault erorr when file path wrong

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 // function to cmpute file size
 int file_size(char file_name[30])
 {
- FILE *fp;
- fp = fopen(file_name, "r");
+ FILE *fp; // make pointer.
+ fp = fopen(file_name, "r"); // open file in read mode.
  int perv = ftell(fp); // get pointer location.
  fseek(fp, 0, SEEK_END); // seek to end of file.
  int size = ftell(fp); // get current file pointer.
  fseek(fp, perv, SEEK_SET); // seek back to last location of pointer.
  return size;
 }
-
-void check_arguments(int argc)
+void check_arguments(int argc,char file_name[20])
 {
   // if argument's smaller than 3 print guid to use and exit 
   if(argc < 3)
@@ -20,20 +20,22 @@ void check_arguments(int argc)
     printf("USE [FILE_NAME] [OUT_FILE_NAME] [PASSWORD] ! THE PASSWORD TO BE A NUMBER \n");
     exit(1);
   }
+  if(access(file_name , R_OK))
+  {
+   printf("can't access to file %s .\n" , file_name);
+   exit(1);
+  }
 }
 
 int main(int argc , char *argv[])
 {
-  check_arguments(argc);
+  check_arguments(argc , argv[1]);
   // define var's for handel ...
-  FILE *fptr; // define file pointer 
   char *data; // for save data
-  int byte_of_file = file_size(argv[1]); // get file size
-  printf("%d" , byte_of_file); // print file size for debug
+  int byte_of_file = file_size(argv[1]); // get file sizeS
   data = (char*)malloc(byte_of_file * sizeof(char)); // malloc memory for data
-  printf("file opened ");  
+  FILE *fptr; // define file pointer 
   fptr = fopen(argv[1] , "rb"); // open file for read binery  
-
   int i = 0 ; // index for remember data index
   while (!feof(fptr)) // if end of continu  
   {
